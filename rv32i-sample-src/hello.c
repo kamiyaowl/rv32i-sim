93b46f4a64f34b1bde7cd972907b14699200f080
K <kamiyaowl@gmail.com>
2019-04-24 01:12:52 +0900

    new file:   rv32i-sample-src/docker-compose.yml

---
+++ rv32i-sample-src/hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    putchar('H');
+    putchar('e');
+    putchar('l');
+    putchar('l');
+    putchar('o');
+    putchar(' ');
+    putchar('W');
+    putchar('o');
+    putchar('r');
+    putchar('l');
+    putchar('d');
+    putchar('!');
+    putchar('\n');
+    return 0;
+}

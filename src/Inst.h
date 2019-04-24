eaab80d5408db2fab2e9a07b96d6f3fba348b783
K <kamiyaowl@gmail.com>
2019-04-25 02:05:14 +0900

    modified:   src/Inst.h

--- src/Inst.h
+++ src/Inst.h
@@ -1,11 +1,19 @@
 #include <string>
 #include <stdint.h>
+#include <functional>
+#include <bitset>
 
 using namespace std;
 
 namespace sim {
+    // TODO: 命令エンコーディングによってかっこよく分解して
     enum ImmType: char {
         R='R', I='I', S='S', U='U',
+    };
+    struct Args {
+        uint8_t opcode;
+        uint8_t rs1;
+        uint8_t rs2;
     };
 
     template <typename T>
@@ -14,5 +22,6 @@ namespace sim {
             string name;
             uint32_t opcode;
             ImmType immType;
+            function<
     };
 };

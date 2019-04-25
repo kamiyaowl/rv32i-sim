aa0ba5697dd05d15e867085c965f0aabdfdaaa23
K <kamiyaowl@gmail.com>
2019-04-26 01:42:13 +0900

    Registerのコメント注釈を追加

--- src/Reg.h
+++ src/Reg.h
@@ -11,6 +11,19 @@ namespace sim {
     template<typename T, size_t XLEN>
     class Reg {
         protected:
+            // Calling conversion
+            // x0     : zero  : always zero
+            // x1     : ra    : return address
+            // x2     : sp    : stack pointer
+            // x3     : gp    : global pointer
+            // x4     : tp    : thread pointer
+            // x5-7   : t0-2  : temporary register
+            // x8     : s0/fp : saved register / frame pointer
+            // x9     : s1    : saved register
+            // x10-11 : a0-1  : function arguments / return value
+            // x12-17 : a2-7  : function arguments
+            // x18-27 : s2-11 : saved register
+            // x28-31 : t3-6  : temporary registers
             vector<T> x;
             T pc;
         public:

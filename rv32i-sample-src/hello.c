94750b804c8c851a3557d4f37750ea4beecdc662
K <kamiyaowl@gmail.com>
2019-04-29 19:31:26 +0900

    サンプルプログラムを組み込み向けに更新

--- rv32i-sample-src/hello.c
+++ rv32i-sample-src/hello.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
 
+#include <stdint.h>
+
+#define UART_PERIPHERAL_BASE_ADDR (0x10000000)
+
+void uart_tx(const char c) {
+    volatile uint8_t* uartTxPtr = (volatile uint8_t*)UART_PERIPHERAL_BASE_ADDR;
+    // TODO: もし実機を完全に模倣するなら送信バッファFullフラグで待ったりする
+    // TxBufに値を書き込み
+    *uartTxPtr = (uint8_t)c;
+}
+void print(const char* str) {
+    for(int i = 0 ; str[i] != '\0' ; ++i) {
+        uart_tx(str[i]);
+    }
+}
 int main(void) {
-    printf("Hello RISC-V! \n");
+    const char* hello = "Hello RISC-V! \n";
+    print(hello);
+
     return 0;
 }
--- rv32i-sample-src/hello.s
+++ rv32i-sample-src/hello.s
@@ -1,27 +1,81 @@
 	.file	"hello.c"
 	.option nopic
 	.text
+	.align	2
+	.globl	uart_tx
+	.type	uart_tx, @function
+uart_tx:
+	addi	sp,sp,-48
+	sw	s0,44(sp)
+	addi	s0,sp,48
+	mv	a5,a0
+	sb	a5,-33(s0)
+	li	a5,268435456
+	sw	a5,-20(s0)
+	lw	a5,-20(s0)
+	lbu	a4,-33(s0)
+	sb	a4,0(a5)
+	nop
+	lw	s0,44(sp)
+	addi	sp,sp,48
+	jr	ra
+	.size	uart_tx, .-uart_tx
+	.align	2
+	.globl	print
+	.type	print, @function
+print:
+	addi	sp,sp,-48
+	sw	ra,44(sp)
+	sw	s0,40(sp)
+	addi	s0,sp,48
+	sw	a0,-36(s0)
+	sw	zero,-20(s0)
+	j	.L3
+.L4:
+	lw	a5,-20(s0)
+	lw	a4,-36(s0)
+	add	a5,a4,a5
+	lbu	a5,0(a5)
+	mv	a0,a5
+	call	uart_tx
+	lw	a5,-20(s0)
+	addi	a5,a5,1
+	sw	a5,-20(s0)
+.L3:
+	lw	a5,-20(s0)
+	lw	a4,-36(s0)
+	add	a5,a4,a5
+	lbu	a5,0(a5)
+	bnez	a5,.L4
+	nop
+	lw	ra,44(sp)
+	lw	s0,40(sp)
+	addi	sp,sp,48
+	jr	ra
+	.size	print, .-print
 	.section	.rodata
 	.align	2
 .LC0:
-	.string	"Hello RISC-V! "
+	.string	"Hello RISC-V! \n"
 	.text
 	.align	2
 	.globl	main
 	.type	main, @function
 main:
-	addi	sp,sp,-16
-	sw	ra,12(sp)
-	sw	s0,8(sp)
-	addi	s0,sp,16
+	addi	sp,sp,-32
+	sw	ra,28(sp)
+	sw	s0,24(sp)
+	addi	s0,sp,32
 	lui	a5,%hi(.LC0)
-	addi	a0,a5,%lo(.LC0)
-	call	puts
+	addi	a5,a5,%lo(.LC0)
+	sw	a5,-20(s0)
+	lw	a0,-20(s0)
+	call	print
 	li	a5,0
 	mv	a0,a5
-	lw	ra,12(sp)
-	lw	s0,8(sp)
-	addi	sp,sp,16
+	lw	ra,28(sp)
+	lw	s0,24(sp)
+	addi	sp,sp,32
 	jr	ra
 	.size	main, .-main
 	.ident	"GCC: (GNU) 8.3.0"
